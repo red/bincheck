e3acb6f1ccd466cfc3b9c2988d23b79fdd481278
x8x <github@reboot.ch>
2018-06-12 18:43:59 +0700

    initial commit

---
+++ orgs.js
@@ -0,0 +1,12 @@
+const orgs = {
+	red: {
+		name: 'Red Language',
+		url: 'https://www.red-lang.org/',
+		apps: {
+			wallet: {
+				name: 'RED Wallet',
+				url: 'https://github.com/red/wallet'
+			}
+		}
+	}
+}
