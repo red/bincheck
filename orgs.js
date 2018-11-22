40551b79d41f066b6a17fac627dc07d0bfe18c31
x8x <x8x@users.noreply.github.com>
2018-11-22 14:03:08 +0700

    Add Red Toolchain 0.6.4 built at commit 755eb94

--- orgs.js
+++ orgs.js
@@ -3,6 +3,10 @@ const orgs = {
 		name: 'Red Language',
 		url: 'https://www.red-lang.org/',
 		apps: {
+			toolchain: {
+				name: 'Red Toolchain',
+				url: 'https://github.com/red/red'
+			},
 			wallet: {
 				name: 'RED Wallet',
 				url: 'https://github.com/red/wallet'
